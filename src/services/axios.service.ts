0f71c4379d33fcec992f3747a682c0b43db42a19
markkravic <hbklksndr@protonmail.com>
2022-05-30 11:49:55 +0300

    TS_training

---
+++ src/services/axios.service.ts
@@ -0,0 +1,13 @@
+import axios, {AxiosResponse} from "axios";
+
+import {baseURL} from "../constants";
+
+const axiosService = axios.create({baseURL});
+
+export type Res<T> = Promise<AxiosResponse<T>>
+
+export {
+    axiosService
+}
+
+
